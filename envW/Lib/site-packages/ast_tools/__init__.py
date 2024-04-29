87004f32fd4850bbe4bbea95f6373e4d2be32e1a
Miqueias S. Costa <40896649+miqdeveloper@users.noreply.github.com>
2024-04-29 18:19:37 -0300

    Delete envW directory

--- envW/Lib/site-packages/ast_tools/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-ast_tools top level package
-"""
-from .common import *
-from . import immutable_ast
-from . import passes
-from . import stack
-from . import transformers
-from . import visitors
--- envW/Lib/site-packages/ast_tools/transformers/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-NodeTransformers of general utility
-"""
-from .if_inliner import Inliner
-from .loop_unroller import Unroller
-from .node_replacer import NodeReplacer
-from .normalizers import ElifToElse, NormalizeBlocks, NormalizeLines
-from .renamer import Renamer
-from .symbol_replacer import SymbolReplacer
